c444e9ae55869be1ddf1bf789eedca11442d4412
borna471 <104699168+borna471@users.noreply.github.com>
2022-08-08 18:10:10 -0700

    built GUI, added save, load, and 2 features

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -1,7 +1,10 @@
 package ui;
 
+import sun.misc.Launcher;
+
 public class Main {
     public static void main(String[] args) {
         new PlayerStatApp();
+
     }
 }
--- src/main/ui/PlayerStatApp.java
+++ src/main/ui/PlayerStatApp.java
@@ -6,20 +6,38 @@ import model.PlayerList;
 import persistence.JsonReader;
 import persistence.JsonWriter;
 
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.sql.DataTruncation;
 import java.util.List;
 import java.util.Scanner;
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 
 
 // Runs app initializing console with all features using methods from model classes
-public class PlayerStatApp {
+public class PlayerStatApp extends JFrame {
     private static final String JSON_STORE = "./data/CompareList.json";
+    private JsonWriter jsonWriter;
+    private JsonReader jsonReader;
+
+    public static final int WIDTH = 800;
+    public static final int HEIGHT = 800;
+
+    private JButton addPlayer;
+    private JButton loadSavedList;
+    private JButton saveCurrentList;
+    private JButton filterCurrentList;
+    private JTextField addPlayerBox;
+    private JLabel playerBoxLabel;
+    private DefaultListModel cmprList;
+    private JList playerList;
+
     private Scanner input;
     private Player player1;
     private Player player2;
-
     private Player player3;
     private Player player4;
     private Player player5;
@@ -28,20 +46,19 @@ public class PlayerStatApp {
     private CompareList cmpList;
     private CompareList compareList;
 
-    private JsonWriter jsonWriter;
-    private JsonReader jsonReader;
+    JFrame frame = new JFrame();
+    JPanel panel = new JPanel();
 
 
-
-
-    // EFFECTS: runs runApp method
     public PlayerStatApp() {
-        runApp();
+        super("Player Stat App");
 
+        initGraphics();
+        // initApp();
     }
 
     // EFFECTS: intialize console app, bring up needed aspects to get it running
-    public void runApp() {
+    public void initApp() {
 
         boolean keepGoing = true;
         String command = null;
@@ -60,6 +77,201 @@ public class PlayerStatApp {
             }
         }
 
+    }
+
+    private void initGraphics() {
+
+        player1 = new Player("Messi", "att", "winger",
+                11, 14, 34, 9,
+                115, 39, 0, 76, 87,
+                0.32, 0.74, 0.68, 2.56);
+
+        player2 = new Player("Ronaldo", "att", "striker",
+                24, 3, 39, 21,
+                133, 55, 0, 36, 30,
+                0.62, 0.69, 0.62, 0.77);
+
+        player3 = new Player("Benzema", "att", "centre-forward",
+                42, 13, 44, 33,
+                174, 81, 0, 0, 75,
+                1.01, 1.34, 1.08, 1.22);
+
+        player4 = new Player("Lewandowski", "att", "striker",
+                50, 6, 46, 42,
+                191, 96, 0, 0, 61,
+                1.13, 1.26, 1.08, 1.01);
+
+        player5 = new Player("Salah", "att", "winger",
+                31, 15, 51, 25,
+                184, 70, 0, 0, 71,
+                0.70, 1.03, 0.90, 1.96);
+
+        plrList = new PlayerList();
+        plrList.getPlayerList().add(player1);
+        plrList.getPlayerList().add(player2);
+        plrList.getPlayerList().add(player3);
+        plrList.getPlayerList().add(player4);
+        plrList.getPlayerList().add(player5);
+
+        cmprList = new DefaultListModel();
+        // cmprList.addElement(player1);
+        // cmprList.addElement(player2);
+
+        jsonWriter = new JsonWriter(JSON_STORE);
+        jsonReader = new JsonReader(JSON_STORE);
+
+        frame.setLayout(new BorderLayout());
+        frame.setSize(WIDTH, HEIGHT);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+        frame.setTitle("Player Stat App");
+
+        panel.setBorder(BorderFactory.createEmptyBorder(200, 200, 200, 200));
+        panel.setLayout(new GridLayout(0, 1));
+        // frame.setContentPane(panel);
+        
+        displayMenuGUI();
+
+    }
+
+    private void displayMenuGUI() {
+
+        frame.add(panel, BorderLayout.CENTER);
+
+        JTextArea players = new JTextArea("Compare player statistics below!\n");
+        players.setEditable(false);
+        frame.add(players, BorderLayout.NORTH);
+        players.setVisible(true);
+
+        loadSavedList = new JButton("Load saved list");
+        loadSavedList.setPreferredSize(new Dimension(70, 70));
+        panel.add(loadSavedList, CENTER_ALIGNMENT);
+        loadSavedList.setVisible(true);
+        loadSavedList.addActionListener(new LoadListListener());
+
+        saveCurrentList = new JButton("Save Current List");
+        panel.add(saveCurrentList, CENTER_ALIGNMENT);
+        saveCurrentList.setVisible(true);
+        saveCurrentList.addActionListener(new SaveListListener());
+
+        addPlayer = new JButton("Add Player");
+        addPlayer.setPreferredSize(new Dimension(100, 100));
+        panel.add(addPlayer, CENTER_ALIGNMENT);
+        addPlayer.setVisible(true);
+        addPlayer.addActionListener(new AddPlayerListener());
+
+        filterCurrentList = new JButton("Remove Player");
+        panel.add(filterCurrentList, CENTER_ALIGNMENT);
+        filterCurrentList.setVisible(true);
+        filterCurrentList.addActionListener(new RemovePlayerListener());
+
+        addPlayerBox = new JTextField(20);
+        addPlayerBox.setBounds(100, 20, 165, 25);
+        panel.add(addPlayerBox);
+
+        playerBoxLabel = new JLabel("Type a name, then press the button above to add to your list!");
+        panel.add(playerBoxLabel);
+
+        playerList = new JList(cmprList);
+        playerList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        playerList.setSelectedIndex(0);
+        playerList.setVisibleRowCount(3);
+        JScrollPane listScrollPane = new JScrollPane(playerList);
+
+        frame.add(playerList, BorderLayout.SOUTH);
+
+
+
+        // System.out.println(addPlayerBox.getText());
+
+
+    }
+
+
+
+    private class AddPlayerListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            String plr = addPlayerBox.getText();
+
+            for (Player player : plrList.getPlayerList()) {
+                if (player.getName().equals(plr)) {
+                    cmprList.add(cmprList.size(),
+                            player.getName() + " "
+                            + player.getCat() + " "
+                            + player.getPost() + " "
+                                    + player.getGs() + " "
+                                    + player.getAst() + " "
+                                    + player.getMts());
+                }
+            }
+
+        }
+    }
+
+    private class RemovePlayerListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            // String post = addPlayerBox.getText();
+
+            int index = playerList.getSelectedIndex();
+
+            cmprList.remove(index);
+
+        }
+    }
+
+    private class LoadListListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            try {
+                for (Player player : jsonReader.read().getCompareList()) {
+                    cmprList.addElement(player.getName() + " "
+                            + player.getCat() + " "
+                            + player.getPost() + " "
+                            + player.getGs() + " "
+                            + player.getAst() + " "
+                            + player.getMts());
+                }
+                System.out.println("Loaded list from " + JSON_STORE);
+            } catch (IOException ioException) {
+                System.out.println("Unable to read from file: " + JSON_STORE);
+            }
+
+        }
+    }
+
+    private class SaveListListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+            try {
+                CompareList guiCompareList = new CompareList();
+
+                for (Object element : cmprList.toArray()) {
+                    String element2 = element.toString().substring(0, element.toString().indexOf(" "));
+                    
+                    Player player = null;
+                    
+                    for (Player plr : plrList.getPlayerList()) {
+                        if (plr.getName().equals(element2)) {
+                            player = plr;
+                        }
+                    }
+
+                    guiCompareList.addPlayer(player);
+                }
+
+                jsonWriter.open();
+                jsonWriter.write(guiCompareList);
+                jsonWriter.close();
+                System.out.println("Saved list to " + JSON_STORE);
+            } catch (FileNotFoundException fileNotFoundException) {
+                System.out.println("Unable to write to file: " + JSON_STORE);
+            }
+
+        }
     }
 
     // EFFECTS: process command from user to take to other parts of app
@@ -112,8 +324,66 @@ public class PlayerStatApp {
 
         System.out.println("add players to the comparison list!");
 
-        loadtool();
 
+        System.out.println("Select from:");
+        System.out.println("\tload -> load your previous list");
+        System.out.println("\tnew -> start a new list");
+        System.out.println("\tback -> return back to main menu");
+
+        String command = input.next();
+
+        if (command.equals("load")) {
+            loadtool();
+        } else if (command.equals("new")) {
+            newListTool();
+        } else {
+            init();
+        }
+//
+//
+//
+//        loadtool();
+//
+//        System.out.println("enter a player's name to add them to the list (capitalize as with regular names)");
+//        plr = input.next();
+//
+//
+//
+//        for (Player player : plrList.getPlayerList()) {
+//            if (player.getName().equals(plr)) {
+//                cmpList.addPlayer(player);
+//            }
+//        }
+//
+////        System.out.println("would you like to add another player? (y/n)");
+////        String answr;
+////
+////        answr = input.next();
+//
+//        String plr2;
+//
+//        System.out.println("\nenter a second player's name to add them to the list
+//        (capitalize as w regular names)\n");
+
+//        plr2 = input.next();
+//
+//        for (Player player : plrList.getPlayerList()) {
+//            if (player.getName().equals(plr2)) {
+//                cmpList.addPlayer(player);
+//            }
+//        }
+//
+//
+//        displayPlayerList(cmpList.getCompareList());
+
+        closeFunction();
+
+        return cmpList;
+
+    }
+
+    private void newListTool() {
+        String plr;
         System.out.println("enter a player's name to add them to the list (capitalize as with regular names)");
         plr = input.next();
 
@@ -124,11 +394,6 @@ public class PlayerStatApp {
                 cmpList.addPlayer(player);
             }
         }
-
-//        System.out.println("would you like to add another player? (y/n)");
-//        String answr;
-//
-//        answr = input.next();
 
         String plr2;
 
@@ -144,25 +409,19 @@ public class PlayerStatApp {
 
         displayPlayerList(cmpList.getCompareList());
 
-        closeFunction();
-
-        return cmpList;
-
     }
 
     // EFFECTS: loads prompts and runs methods for loading previous saves if needed
     private void loadtool() {
-        String load;
-        System.out.println("would you like to load your previously saved list? (y/n)");
-        load = input.next();
 
-        if (load.equals("y")) {
-            loadCompareList();
-            System.out.println("\n");
-            displayPlayerList(cmpList.getCompareList());
-            System.out.println("your previous list is loaded!\n");
-            init();
-        }
+        loadCompareList();
+        System.out.println("\n");
+        displayPlayerList(cmpList.getCompareList());
+        System.out.println("your previous list is loaded!\n");
+//        System.out.println("\nenter any key to return to main menu\n");
+//        input.next();
+//        init();
+
     }
 
     // EFFECTS: prompts and processes answers to end the compList function page
@@ -295,5 +554,18 @@ public class PlayerStatApp {
         input.useDelimiter("\n");
     }
 
+    public class SplashScreen extends JWindow {
+        private static final long serialVersionUID = 1L;
+        private JPanel contentPane;
+
+        public SplashScreen(JFrame frame, String msg) {
+            setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            setBounds(100, 100, 100, 100);
+            contentPane = new JPanel();
+            contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+            contentPane.setLayout(new BorderLayout(0, 0));
+            setContentPane(contentPane);
+        }
+    }
 
 }
